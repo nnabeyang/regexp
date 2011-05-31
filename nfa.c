b45af9ce0dc7b1dbdd101dc370524620e1d6c61b
noriaki watanabe <nabeyang@gmail.com>
2011-05-31 11:02:54 +0900

    2nd commit

--- nfa.c
+++ nfa.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
 char* reg2post(const char* re) {
 static char buf[8000];
 char* dst;
@@ -17,15 +18,40 @@ int i = 0;
     *dst++ = '.';
   return buf;
 }
-
+struct State {
+  int c;
+  struct State* out;
+};
+struct State* state(int c, struct State* out) {
+struct State* s = malloc(sizeof(struct State));
+  s->c = c; s->out = out;
+  return s;
+}
 void test(void);
 int main(void) {
   test();
   return 0;
 }
-void test(void) {
+void test_reg2post(void) {
   assert(!strcmp("a",reg2post("a")) && "a -> a");
   assert(!strcmp("ab.",reg2post("ab")) && "ab -> ab.");
   assert(!strcmp("ab.c.d.e.f.g.",reg2post("abcdefg")) && "abcdefg -> ab.c.d.e.f.g.");
 }
-
+const char* CheckState(struct State* s, FILE* fp) {
+static char buf[80];
+  if(fp) 
+    fprintf(fp, "c:%c, out:%s\n", (char)s->c, (s->out)? "NN": "NULL");   
+  sprintf(buf, "c:%c, out:%s\n", (char)s->c, (s->out)? "NN": "NULL");
+  return buf; 
+}
+void test_create_state(void) {
+  struct State* s1 = state('a', NULL);
+  struct State* s2 = state('b', s1);
+  assert(!strcmp("c:a, out:NULL\n",CheckState(s1, NULL)));
+  assert(!strcmp("c:b, out:NN\n",CheckState(s2, NULL)));
+  free(s1);free(s2);
+}
+void test(void) {
+  test_reg2post();
+  test_create_state();
+}
