afd46acc893a80e1463a02f02841891e49b8241e
noriaki watanabe <nabeyang@gmail.com>
2011-06-01 13:28:45 +0900

    13th commit

--- nfa.c
+++ nfa.c
@@ -1,3 +1,17 @@
+/*
+ * Regular expression implementation.
+ * Supports only ( | ) * + ?.  No escapes.
+ * Compiles to NFA and then simulates NFA
+ * using Thompson's algorithm.
+ *
+ * See also http://swtch.com/~rsc/regexp/ and
+ * Thompson, Ken.  Regular Expression Search Algorithm,
+ * Communications of the ACM 11(6) (June 1968), pp. 419-422.
+ * 
+ * Copyright (c) 2007 Russ Cox.
+ * Can be distributed under the MIT license, see bottom of file.
+ */
+
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
@@ -525,3 +539,28 @@ void test(void) {
   test_match_plus();
   test_match_quest();
 }
+
+/*
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall
+ * be included in all copies or substantial portions of the
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
